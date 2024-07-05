c012a6f874edb763f336a7e5a95451b6ad74ad5a
Mathis Charretier <charretiermathis@gmail.com>
2024-07-05 15:10:52 +0200

    Fix (packages): delete web-vitals

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import reportWebVitals from './reportWebVitals'
 import { HashRouter as Router, Route, Routes } from 'react-router-dom'
 import { persistor, store } from './redux/store'
 import { PersistGate } from 'redux-persist/integration/react'
@@ -24,8 +23,3 @@ root.render(
         </Provider>
     </React.StrictMode>
 )
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals()
